7c0a422aee060b49d12a3a20571960b3fd8f544f
jibancat <mho026@ucr.edu>
2020-09-17 20:34:21 +0800

    [error handler] for some empty spectra

--- multi_dlas/learn_qso_model_meanflux.m
+++ multi_dlas/learn_qso_model_meanflux.m
@@ -56,6 +56,11 @@ all_lyman_1pzs       = nan(num_forest_lines, num_quasars, num_rest_pixels);
 rest_fluxes          = nan(num_quasars, num_rest_pixels);
 rest_noise_variances = nan(num_quasars, num_rest_pixels);
 
+% [error handler] the preload_qsos should fliter out empty spectra;
+% this line is to prevent there is any empty spectra
+% in preloaded_qsos.mat for some reason, e.g., unmatched filter_flags.
+is_empty             = false(num_quasars, 1);
+
 % interpolate quasars onto chosen rest wavelength grid
 for i = 1:num_quasars
   z_qso = z_qsos(i);
@@ -67,6 +72,14 @@ for i = 1:num_quasars
 
   this_flux(this_pixel_mask)           = nan;
   this_noise_variance(this_pixel_mask) = nan;
+
+  % [error handler] in case filter_flags and test_ind not exactly match
+  fprintf('processing quasar %i with lambda_size = %i %i ...\n', i, size(this_wavelengths))
+
+  if all(size(this_wavelengths) == [0 0])
+    is_empty(i, 1) = 1;
+    continue;
+  end
 
   this_rest_wavelengths = emitted_wavelengths(this_wavelengths, z_qso);
 
@@ -104,6 +117,14 @@ for i = 1:num_quasars
       interp1(this_rest_wavelengths, this_noise_variance, rest_wavelengths);
 end
 clear('all_wavelengths', 'all_flux', 'all_noise_variance', 'all_pixel_mask');
+
+% [error handler] filter out empty spectra
+% note: if you've done this in preload_qsos then skip these lines
+z_qsos               = z_qsos(~is_empty);
+lya_1pzs             = lya_1pzs(~is_empty, :);
+rest_fluxes          = rest_fluxes(~is_empty, :);
+rest_noise_variances = rest_noise_variances(~is_empty, :);
+all_lyman_1pzs       = all_lyman_1pzs(:, ~is_empty, :);
 
 % mask noisy pixels
 ind = (rest_noise_variances > max_noise_variance);
