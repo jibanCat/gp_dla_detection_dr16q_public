a3df95ead7c44863e4ef131a8f78d42c58d1a3dd
jibancat <mho026@ucr.edu>
2020-09-24 08:42:47 +0800

    add a learning script submission file

---
+++ submit/run_learn.m
@@ -0,0 +1,16 @@
+% run_learn.m : learn a GP null model from a subset of
+% DR12Q in DR16Q spectra.
+
+cd ..
+
+addpath multi_dlas/
+addpath dr16q/
+
+set_parameters_multi;
+
+cd minFunc_2012/
+addpath(genpath(pwd));
+mexAll;
+
+training_set_name = 'dr16q_minus_dr12q_gp';
+learn_qso_model_meanflux_dr16q;
